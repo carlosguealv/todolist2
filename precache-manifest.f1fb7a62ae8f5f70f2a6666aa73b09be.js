26dfbc4f3d66bfbc8ecd5d62a7389a7a8a9aba6d
porfavorde <carlosteniswarrior@gmail.com>
2020-03-25 10:09:00 +0100

    Updates

---
+++ precache-manifest.f1fb7a62ae8f5f70f2a6666aa73b09be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567107a34779ca8ed90bae6d0dc32070",
+    "url": "/todolist2/index.html"
+  },
+  {
+    "revision": "03de3f0daa6c30ec85c0",
+    "url": "/todolist2/static/css/main.70b7ac24.chunk.css"
+  },
+  {
+    "revision": "a3f1f3db916bc0a75071",
+    "url": "/todolist2/static/js/2.7447f126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist2/static/js/2.7447f126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03de3f0daa6c30ec85c0",
+    "url": "/todolist2/static/js/main.138d7258.chunk.js"
+  },
+  {
+    "revision": "4834ad94010774455b8a",
+    "url": "/todolist2/static/js/runtime-main.cec5ec71.js"
+  }
+]);
